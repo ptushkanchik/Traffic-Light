57e29a7cd375f40553383b42e7be2a6e4df2c2d3
ptushkanchik <ptushkin83@mail.ru>
2016-02-15 23:43:36 +0200

    Function from index.html has been include here

--- functions.js
+++ functions.js
@@ -1,10 +1,54 @@
+//время работы в мигающем режиме
+var blinkTime = 3;
+//время работы желтого фонаря
+var yellowTime = 3; 
+
+function trafficLightStart(){
+	
+		if( input_red.value=="0" || input_green.value=="0" || isNaN(input_red.value) || isNaN(input_green.value) || +input_green.value<4 || +input_red.value<4 ){
+			alert("Вводите данные корректно - в числовом виде и во все поля. Значения должны быть больше 3, меньше 999 и являться положительными числами.");
+			input_red.value="0";
+			input_green.value="0";
+			return;
+			}
+	
+	//заблокировать поля ввода 
+		input_red.disabled = true;
+		input_green.disabled = true;
+		start.disabled = true;
+		
+	
+	
+	 setTimeout(function cicle(){
+		
+			green.point(+input_green.value);
+			timer.runTime("green", +input_green.value);
+			
+			setTimeout(function(){
+				green.blink("green", blinkTime);
+				setTimeout(function(){
+					yellow.point(yellowTime);
+					setTimeout(function(){
+						red.point(+input_red.value);
+						timer.runTime("red", +input_red.value);
+							setTimeout(function(){
+								yellow.point(yellowTime);				
+								},(+input_red.value - yellowTime)*1000);
+								setTimeout(cicle,(+input_red.value)*1005);
+						},(yellowTime)*1000);
+					},( blinkTime)*1000);
+				},+input_green.value*1000);
+	}, 0);
+}
+
+
+
 Element.prototype.point = function(time){
 	var self = this;
 	self.style.backgroundColor = this.id;
 	var timeId = setTimeout(function(){
 		self.style.backgroundColor="";
 		}, time*1000);
-		
 };
 
 Element.prototype.runTime = function(colorText, time){
@@ -24,7 +68,7 @@ Element.prototype.runTime = function(colorText, time){
 			clearTimeout(timerId);
 			self.innerHTML = "";
 		}
-		},1000);
+	},1000);
 }
 
 Element.prototype.blink = function(color, count){
