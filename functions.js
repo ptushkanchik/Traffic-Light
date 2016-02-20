cf1452a902ded118efab5e9c606166dcdb8f5561
ptushkanchik <ptushkin83@mail.ru>
2016-02-20 23:08:54 +0200

    Elements from functions has been replaced by elements from jQuery.

--- functions.js
+++ functions.js
@@ -4,20 +4,19 @@ var blinkTime ;
 var yellowTime; 
 //переменная-переключатель состояния светофора "рабочее/нерабочее"
 var work ;
+
 function trafficLightStart(){
 	
 		if( input_red.value=="0" || input_green.value=="0" || isNaN(input_red.value) || isNaN(input_green.value) || +input_green.value<4 || +input_red.value<4 ){//если введенные данные равны нулю, не числа или меньше 4
 			alert("Вводите данные корректно - в числовом виде и во все поля. Значения должны быть больше 3, меньше 999 и являться положительными числами.");
-			input_red.value="0";//обнулить поле ввода
-			input_green.value="0";//обнулить поле ввода
+			$(".set_time").val("0");//обнулить поля ввода
 			return;//прекратить выполнение функции
 			}
 	
 	//заблокировать поля ввода 
-		input_red.disabled = true;
-		input_green.disabled = true;
+		$(".set_time").prop("disabled", true);
 	//заблокировать кнопку старт
-		start.disabled = true;
+		$("#start").prop("disabled", true);
 		
 	work = true;//переключатель установить в рабочее состояние	
 	blinkTime = 3;//установить время мигания светофора
@@ -62,18 +61,13 @@ function trafficLightStop(){
 	work=false;//поставить переключатель в положение "не работает"
 	blinkTime = 0;//обнулить время мигания
 	yellowTime = 0;//обнулить время работы желтого фонаря
-	input_green.value = 0;//обнулить поле ввода работы зеленого фонаря
-	input_red.value = 0;//обнулить поле ввода работы красного фонаря
-	input_red.disabled = false;//разблокировать поле ввода для красного фонаря 
-	input_green.disabled = false;//обнулить поле ввода для зеленого фонаря
-	start.disabled = false;//разблокировать кнопку "СТАРТ"
-//снять закрашивание всех фонарей и убрать тайиер из поля отсчета времени
+	$(".set_time").val("0");//обнулить поля ввода
+	$(".set_time").prop('disabled', false);//разблокировать поля ввода
+	$("#start").prop('disabled', false);//разблокировать кнопку "СТАРТ"
+//снять закрашивание всех фонарей и убрать таймер из поля отсчета времени
 	setTimeout(function(){
 		timer.innerHTML = "";
-		timer.style.backgroundColor = "";
-		red.style.backgroundColor = "";
-		yellow.style.backgroundColor = "";
-		green.style.backgroundColor = "";
+		$(".visor").css("backgrounColor", "");
 	},1000);
 	
 	
@@ -89,9 +83,7 @@ Element.prototype.point = function(time){
 
 Element.prototype.runTime = function(colorText, time){
 	this.style.color = colorText;
-	this.style.fontSize = "60px";
-	this.style.textAlign = "center";
-	this.style.lineHeight = "100px";
+	$(this).css({fontSize:"60px", textAlign:"center", lineHeight:"100px"});
 	this.innerHTML = time;
 	var self = this;
 	var count = time;
