2fd1208f02568458d9b7d5e36906d16a1951502e
ptushkanchik <ptushkin83@mail.ru>
2016-03-14 11:14:28 +0200

    Properties CSS in functions have been replaced by jQuery's methods

--- functions.js
+++ functions.js
@@ -8,7 +8,7 @@ var ctrl1, ctrl2, ctrl3, ctrl4, ctrl5, ctrl6, ctrl7, ctrl8, ctrl9, ctrl10;
 
 function trafficLightStart(){
 	
-		if( input_red.value=="0" || input_green.value=="0" || isNaN(input_red.value) || isNaN(input_green.value) || +input_green.value<4 || +input_red.value<4 ){//если введенные данные равны нулю, не числа или меньше 4
+		if(isNaN(input_red.value) || isNaN(input_green.value) || +input_green.value<4 || +input_red.value<4 ){//если введенные данные  не числа или меньше 4
 			alert("Вводите данные корректно - в числовом виде и во все поля. Значения должны быть больше 3, меньше 999 и являться положительными числами.");
 			$(".set_time").val("0");//обнулить поля ввода
 			return;//прекратить выполнение функции
@@ -39,7 +39,7 @@ function trafficLightStart(){
 							yellow.point(yellowTime);
 							},(+input_red.value - yellowTime)*1000);
 								
-							ctrl6 =  setTimeout(cicle,(+input_red.value)*1005);
+							ctrl6 =  setTimeout(cicle,(+input_red.value)*1000+100);
 								
 					},(yellowTime)*1000);
 				},( blinkTime)*1000);
@@ -49,8 +49,7 @@ function trafficLightStart(){
 }
 
 function trafficLightStop(){
-	$(".set_time").val("0");//обнулить поля ввода
-	$(".set_time").prop('disabled', false);//разблокировать поля ввода
+	$(".set_time").val("0").prop('disabled', false);//обнулить И разблокировать поля ввода
 	$("#start").prop('disabled', false);//разблокировать кнопку "СТАРТ"
 //остановить все запланированные таймауты
 	clearTimeout(ctrl1);
@@ -64,33 +63,32 @@ function trafficLightStop(){
 	clearTimeout(ctrl9);
 	clearTimeout(ctrl10);
 //снять закрашивание всех фонарей и убрать таймер из поля отсчета времени
-	$(".lamp").css("backgroundColor","");
-	timer.innerHTML = "";
+	$(".lamp").css("backgroundColor","").text("");
+	
 }
 
 
 Element.prototype.point = function(time){
 	var self = this;
-	self.style.backgroundColor = this.id;
+	$(self).css("backgroundColor", this.id);
 	ctrl7 = setTimeout(function(){
-		self.style.backgroundColor="";
+		$(self).css("backgroundColor","");
 		}, time*1000);
 };
 
 Element.prototype.runTime = function(colorText, time){
-	this.style.color = colorText;
-	$(this).css({fontSize:"60px", textAlign:"center", lineHeight:"100px"});
-	this.innerHTML = time;
+	
+	$(this).css({fontSize:"60px", textAlign:"center", lineHeight:"100px", color:colorText}).text(time);
 	var self = this;
 	var count = time;
 	ctrl8 = setTimeout(function backTime(){
 		if(count!=1){
 			count--;
-			self.innerHTML = count;
+			$(self).text(count);
 			ctrl9 =	setTimeout(backTime, 1000);
 		}else{
 			clearTimeout(ctrl8);
-			self.innerHTML = "";
+			$(self).text("");
 		}
 	},1000);
 }
@@ -101,11 +99,11 @@ Element.prototype.blink = function(color, count){
 	var timeId = setTimeout(function elementBlink(){
 		if(count!=0){
 			if(self.style.backgroundColor!=color){
-				self.style.backgroundColor = color;
+				$(self).css("backgroundColor", color);
 				count--;
 				ctrl10 = setTimeout(elementBlink, 500);
 			}else{
-				self.style.backgroundColor = "";
+				$(self).css("backgroundColor", "");
 				count--;
 				ctrl10 =	setTimeout(elementBlink, 500);
 			}	
